df01b6b43bcd51e7d458b38bc7075378184a94ac
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-17 15:02:00 +0545

    login functionality completed

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 const { logger } = require("../utils/logger")
-const {userRegisterValidation} = require('../utils/user.register.validation')
+const {userRegisterValidation, loginValidation} = require('../utils/user.register.validation')
 const User = require('../models/user.models')
 const { upload } = require("../utils/cloudinary")
+const { generateToken } = require("../utils/generateToken")
 
 
 
@@ -29,6 +30,9 @@ const register = async(req,res,next)=>{
         const result = await upload(req.file)
         
        const newuser = await User.create({...req.body,profilePic:result.secure_url,profilePicId:result.public_id})
+       const {accessToken} = generateToken(user)
+        user.accessToken = accessToken
+        await user.save()
        res.status(200).json({success:true,newuser})
     } catch (error) {
         logger.error('registration failed',error.message)
@@ -40,4 +44,48 @@ const register = async(req,res,next)=>{
     }
 }
 
-module.exports = {register}
+const login = async(req,res,next)=>{
+    logger.info("login started...")
+    try {
+        const {error} = loginValidation(req.body)
+        if (error) {
+            logger.warn("validation failed",error.details[0].message)
+            return res.status(400).json({
+                success:false,
+                message:error.details[0].message
+            })
+        }
+        const user = await User.findOne({
+                email:req.body.email
+            }
+        )
+        if (!user) {
+            return res.status(400).json({
+                success:false,
+                message:"invalid credentials"
+            })
+        }
+       const isMatched = await user.comparePassword(req.body.password)
+       if (!isMatched) {
+        return res.status(400).json({
+            success:false,
+            message:"invalid credentials"
+        })
+     }
+        const {accessToken} = generateToken(user)
+        user.accessToken = accessToken
+        await user.save()
+       res.status(200).json({success:true,message:'login successfully'})
+    } catch (error) {
+        logger.error('login failed',error.message)
+        next(error)
+        return res.status(500).json({
+            success:false,
+            message:'User login failed'
+        })
+    }
+}
+
+
+
+module.exports = {register,login}
