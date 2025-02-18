7dafdf29a3104b28ec1a55e1b1c764103eb44914
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-18 12:07:54 +0545

    logout functionality added

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -30,9 +30,6 @@ const register = async(req,res,next)=>{
         const result = await upload(req.file)
         
        const newuser = await User.create({...req.body,profilePic:result.secure_url,profilePicId:result.public_id})
-       const {accessToken} = generateToken(user)
-        user.accessToken = accessToken
-        await user.save()
        res.status(200).json({success:true,newuser})
     } catch (error) {
         logger.error('registration failed',error.message)
@@ -47,6 +44,7 @@ const register = async(req,res,next)=>{
 const login = async(req,res,next)=>{
     logger.info("login started...")
     try {
+        console.log(req.body)
         const {error} = loginValidation(req.body)
         if (error) {
             logger.warn("validation failed",error.details[0].message)
@@ -55,21 +53,18 @@ const login = async(req,res,next)=>{
                 message:error.details[0].message
             })
         }
-        const user = await User.findOne({
-                email:req.body.email
-            }
-        )
+        const user = await User.findOne({email:req.body.email})
         if (!user) {
             return res.status(400).json({
                 success:false,
-                message:"invalid credentials"
+                message:"user invalid credentials"
             })
         }
        const isMatched = await user.comparePassword(req.body.password)
        if (!isMatched) {
         return res.status(400).json({
             success:false,
-            message:"invalid credentials"
+            message:"password invalid credentials"
         })
      }
         const {accessToken} = generateToken(user)
@@ -86,6 +81,27 @@ const login = async(req,res,next)=>{
     }
 }
 
+const logout = async(req,res,next)=>{
+    logger.info("logout started...")
+    try {
+        console.log(req.body)
+        const user = await User.findOne({
+                email:req.body.email
+            }
+        )
+        user.accessToken = ""
+        await user.save()
+        res.status(200).json({success:true,message:'logout successfully'})
+    } catch (error) {
+        logger.error('logout failed',error.message)
+        next(error)
+        return res.status(500).json({
+            success:false,
+            message:'User logout failed'
+        })
+    }
+}
 
 
-module.exports = {register,login}
+
+module.exports = {register,login,logout}
