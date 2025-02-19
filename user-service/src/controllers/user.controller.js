0be9ef9de58e186b0e04ab13717ada251e8649ba
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-19 12:35:17 +0545

    user profile edit functionality

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const { logger } = require("../utils/logger")
 const {userRegisterValidation, loginValidation} = require('../utils/user.register.validation')
 const User = require('../models/user.models')
-const { upload } = require("../utils/cloudinary")
+const { upload, deleteImage } = require("../utils/cloudinary")
 const { generateToken } = require("../utils/generateToken")
 
 
@@ -16,6 +16,9 @@ const register = async(req,res,next)=>{
                 success:false,
                 message:error.details[0].message
             })
+        }
+        if(!req.file){
+            return res.status(400).json({ message: 'No file uploaded' });
         }
         const user = await User.findOne({
                 email:req.body.email
@@ -44,7 +47,6 @@ const register = async(req,res,next)=>{
 const login = async(req,res,next)=>{
     logger.info("login started...")
     try {
-        console.log(req.body)
         const {error} = loginValidation(req.body)
         if (error) {
             logger.warn("validation failed",error.details[0].message)
@@ -70,7 +72,7 @@ const login = async(req,res,next)=>{
         const {accessToken} = generateToken(user)
         user.accessToken = accessToken
         await user.save()
-       res.status(200).json({success:true,message:'login successfully'})
+       res.status(200).json({success:true,accessToken:accessToken,message:'login successfully'})
     } catch (error) {
         logger.error('login failed',error.message)
         next(error)
@@ -84,11 +86,8 @@ const login = async(req,res,next)=>{
 const logout = async(req,res,next)=>{
     logger.info("logout started...")
     try {
-        console.log(req.body)
-        const user = await User.findOne({
-                email:req.body.email
-            }
-        )
+        
+        const user = await User.findById(req.user.userId)
         user.accessToken = ""
         await user.save()
         res.status(200).json({success:true,message:'logout successfully'})
@@ -102,6 +101,69 @@ const logout = async(req,res,next)=>{
     }
 }
 
+const editUserProfile = async(req,res)=>{
+    logger.info("user edit started...")
+    try {
+        const user = await User.findById(req.user.userId)
+            if (!user) {
+                return res.status(400).json({
+                    success:false,
+                    message:"user doesn't exist"
+                })
+            }
+        if(req.file){
+            if(user.profilePic){
+                await deleteImage(user.profilePicId)
+            }
+            
+            //update user
+            const result = await upload(req.file)
+            
+            user.profilePic = result.secure_url
+            user.profilePicId=result.public_id
+            
+        }
+        const {username,firstName,lastName}=req.body
+        username && (user.username = username)
+        firstName && (user.firstName = firstName)
+        lastName && (user.lastName = lastName)
+        await user.save()
+        res.status(200).json({success:true,message:'user updated successfully',user:user})
+    } catch (error) {
+        logger.error('update failed',error.message)
+        return res.status(500).json({
+            success:false,
+            message:'User update failed'
+        })
+    }
+}
 
 
-module.exports = {register,login,logout}
+const deleteUser = async(req,res)=>{
+    logger.info("user deletion started...")
+    try {
+        
+        const user = await User.findOne({email:req.user.email})
+        if (!user) {
+            return res.status(400).json({
+                success:false,
+                message:"user doesn't exist"
+            })
+        }
+        
+        await user.deleteOne()
+        await deleteImage(user.profilePicId)
+        res.status(200).json({success:true,message:'user deleted successfully'})
+    } catch (error) {
+        logger.error('deletion failed',error.message)
+        return res.status(500).json({
+            success:false,
+            message:'User deletion failed'
+        })
+    }
+}
+
+
+
+
+module.exports = {register,login,logout,deleteUser,editUserProfile}
