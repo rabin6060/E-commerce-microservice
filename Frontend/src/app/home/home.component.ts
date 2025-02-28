f354e1cdb9978df5425b3cb72cfe23ac500a6c29
Rabin Mainali <rabinmainali1999@gmail.com>
2025-02-28 20:13:50 +0545

    products fetch added on intial home loading

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  response = signal<Array<Product> | null>([])
+  constructor(private product:ProductService){}
+  ngOnInit(): void {
+    this.fetchProducts()
+  }
+  fetchProducts(){
+    this.product.fetchProducts().subscribe({
+      next:(value:any)=>{
+        this.response.set(value)
+      },
+      error:(err:any)=> {
+        this.response.set(err.error)
+      }
+    }
+    )
+  }
 }
