80c29237e091af201317a94ab687c065283d8170
Rabin Mainali <rabinmainali1999@gmail.com>
2025-03-10 18:57:41 +0545

    finalizing infinite scroll

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
   @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   products = signal<Array<Product> | null>([])
   pageNumber = signal<number>(1)
+  totalPage ?:number
 
   constructor(private product:ProductService){
     
@@ -27,21 +28,23 @@ export class HomeComponent implements OnInit {
     const element = this.scrollContainer.nativeElement
     const atBottom = this.isAtBottom(element)
     if(atBottom){
-      this.pageNumber.update(prev=>prev+1)
-      this.fetchProducts(this.pageNumber())
+      if (this.totalPage && (this.pageNumber() < this.totalPage)) {
+        this.pageNumber.update(prev=>prev+1)
+        this.fetchProducts(this.pageNumber())
+      }
+     
     }
   }
   isAtBottom(element:HTMLElement):boolean{
     const scrollTop = element.scrollTop //how far user scroll
     const scrollHeight = element.scrollHeight //scrollable height
     const clientHeight = element.clientHeight //container height
-    console.log(this.pageNumber())
     return scrollTop+clientHeight >= scrollHeight - 10
   }
   fetchProducts(pageNumber:number){
     this.product.fetchProducts(pageNumber).subscribe({
       next:(value:any)=>{
-        console.log(value)
+        this.totalPage = value.totalPages
         this.products.update((current:any)=>[...current,...value.products])
       },
       error:(err:any)=> {
