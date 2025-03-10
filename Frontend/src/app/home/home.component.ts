c723ef3710be5ffb3c857a1bbd8838b83aafc357
Rabin Mainali <rabinmainali1999@gmail.com>
2025-03-10 18:21:45 +0545

    adding infinite scrolling

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, effect, ElementRef, HostListener, OnInit, signal, ViewChild } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.model';
 import { ProductItemComponent } from "../components/product-item/product-item.component";
@@ -10,16 +10,39 @@ import { ProductItemComponent } from "../components/product-item/product-item.co
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   products = signal<Array<Product> | null>([])
-  constructor(private product:ProductService){}
-  ngOnInit(): void {
-    this.fetchProducts()
+  pageNumber = signal<number>(1)
+
+  constructor(private product:ProductService){
+    
   }
-  fetchProducts(){
-    this.product.fetchProducts().subscribe({
+  
+  ngOnInit(): void {
+    this.fetchProducts(this.pageNumber())
+  }
+
+  @HostListener('scroll',['$event'])
+  onScroll(event:Event){
+    const element = this.scrollContainer.nativeElement
+    const atBottom = this.isAtBottom(element)
+    if(atBottom){
+      this.pageNumber.update(prev=>prev+1)
+      this.fetchProducts(this.pageNumber())
+    }
+  }
+  isAtBottom(element:HTMLElement):boolean{
+    const scrollTop = element.scrollTop //how far user scroll
+    const scrollHeight = element.scrollHeight //scrollable height
+    const clientHeight = element.clientHeight //container height
+    console.log(this.pageNumber())
+    return scrollTop+clientHeight >= scrollHeight - 10
+  }
+  fetchProducts(pageNumber:number){
+    this.product.fetchProducts(pageNumber).subscribe({
       next:(value:any)=>{
         console.log(value)
-        this.products.set(value)
+        this.products.update((current:any)=>[...current,...value.products])
       },
       error:(err:any)=> {
         this.products.set(err.error)
